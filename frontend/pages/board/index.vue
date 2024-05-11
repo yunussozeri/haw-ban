20b6ad36f9b315ea7eea75a4cc5e8d2a723ac81b
wdm373 <167907927+wdm373@users.noreply.github.com>
2024-05-11 10:57:37 +0200

    test1

--- frontend/pages/board/index.vue
+++ frontend/pages/board/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="w-full h-full flex flex-col items-center justify-center">
-    <h1 class="text-4xl font-bold mb-4">This is the board page</h1>
+  <div>
+    <div style="display: flex; align-items: flex-start; justify-content: space-between; margin-top: 150px;">
+      <!-- Platzhalter für die Icons -->
+      <div>
+        <!-- MDI-Icon für den Account -->
+        <i class="mdi mdi-account" style="font-size: 24px;"></i>
+        <!-- MDI-Icon für den Logout -->
+        <i class="mdi mdi-logout" style="font-size: 24px;"></i>
+      </div>
+
+      <!-- Die anderen Menüelemente -->
+      <div style="flex-grow: 1; padding: 20px; background-color: #A7B4CF;">
+        <div style="background-color: #A7B4CF; color: #fff; padding: 10px; border-radius: 5px;">Backlog</div>
+      </div>
+      <div style="flex-grow: 1; padding: 20px; background-color: #A7B4CF;">
+        <div style="background-color: #A7B4CF; color: #fff; padding: 10px; border-radius: 5px;">To Do</div>
+      </div>
+      <div style="flex-grow: 1; padding: 20px; background-color: #A7B4CF;">
+        <div style="background-color: #A7B4CF; color: #fff; padding: 10px; border-radius: 5px;">In Progress</div>
+      </div>
+      <div style="flex-grow: 1; padding: 20px; background-color: #A7B4CF;">
+        <div style="background-color: #A7B4CF; color: #fff; padding: 10px; border-radius: 5px;">Done</div>
+      </div>
+    </div>
   </div>
 </template>
+
