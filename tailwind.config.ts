c9d5849108eb6ef5b6dfe5b5ffcadd5d837d0c8e
yunussozeri <yunus.sozeri254@gmail.com>
2024-05-12 16:54:04 +0200

    aded logo icon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,4 +2,13 @@ import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   content: ["docs/content/**/*.md"],
+  // 50-swn 950-w  kdr aciktan koyuya renkleri yaz
+  theme: {
+    // colors: {
+    //   blue: {},
+    // },
+    fontFamily: {
+      "comic-sans-ms": ["Comic Sans MS"],
+    },
+  },
 };
