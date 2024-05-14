db6a0e9c85ebafb15f54570f612767fd8614cdf7
Nicolas Wenzel <nicolas.l.wenzel@outlook.de>
2024-05-14 13:27:56 +0200

    removed sign-in, added images

--- pages/login.vue
+++ pages/login.vue
@@ -9,21 +9,27 @@ async function signIn() {
   client.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: "http://192.168.2.225:3000/",
+      redirectTo: "localhost:3000/",
     },
   });
 }
 </script>
 <template>
   <div class="flex h-screen w-screen">
-    <div class="flex h-full flex-1 items-center justify-center">
-      <NuxtImg
-        class="w-full"
-        src="https://cataas.com/cat"
-        alt="Startpic"
-        fit="inside"
-      />
+    <div class="flex w-1/2 items-center justify-center">
+      <!-- Set a fixed size for the image container -->
+      <div
+        class="flex h-96 w-96 items-center justify-center overflow-hidden rounded-full"
+      >
+        <!-- Image fills the container and is clipped to the boundary -->
+        <NuxtImg
+          class="h-full w-full object-cover"
+          src="../public/images/loginphoto.svg"
+          alt="Startpic"
+        />
+      </div>
     </div>
+
     <div class="flex h-full flex-1 flex-col items-center justify-center gap-8">
       <NuxtLink to="/">
         <h1 class="font-comic-sans-ms text-4xl font-bold">MY KANBAN BOARD</h1>
