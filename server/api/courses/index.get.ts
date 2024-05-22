9ce7b296b00f2667abd7d6927e0f82700a0c8e9c
Nicolas Wenzel <nicolas.l.wenzel@outlook.de>
2024-05-22 22:11:55 +0200

    removed limit

--- server/api/courses/index.get.ts
+++ server/api/courses/index.get.ts
@@ -10,7 +10,6 @@ export default defineEventHandler(async (event) => {
       kuerzel: courses.kuerzel,
     })
     .from(courses)
-    .limit(10)
     .then((result) => {
       return result;
     });
