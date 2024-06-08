15e72c912cfeefc23d4b0e346e8a541c49c5887e
yunussozeri <yunus.sozeri.254@gmail.com>
2024-06-08 12:11:30 +0200

    formatting and refactoringn login

--- pages/board/index-zahra.vue
+++ pages/board/index-zahra.vue
@@ -1,76 +1,116 @@
 <template>
-    <div class="h-screen">
+  <div class="h-screen">
     <!-- Hintergrund oben mit anderer Farbe -->
-    <div class="bg-blue-900 text-white p-5 relative">
-    <img src="https://raw.githubusercontent.com/wdm373/MyKanbanBoard-Logo/main/Board_Seite-PNG.png"
-    alt="My-KanbanBoard"
-    class="absolute top-0 left-0 w-[200px] h-[180px] mt-[-50px] ml-0">
-    <!-- Flexbox-Container für die Icons oben rechts -->
-    <div class="flex justify-end">
-    <!-- MDI-Icon für den Account -->
-    <UIcon name="i-heroicons-user-solid"
-    class="text-xl cursor-pointer mr-2" @click="handleAccountClick">
-    </UIcon>
-    <!-- MDI-Icon für den Logout -->
-    <UIcon name="i-heroicons-arrow-right-start-on-rectangle-20-solid"
-    class="text-xl cursor-pointer mr-2" @click="handleLogoutClick">
-    </UIcon>
-    </div>
+    <div class="relative bg-blue-900 p-5 text-white">
+      <img
+        src="https://raw.githubusercontent.com/wdm373/MyKanbanBoard-Logo/main/Board_Seite-PNG.png"
+        alt="My-KanbanBoard"
+        class="absolute left-0 top-0 ml-0 mt-[-50px] h-[180px] w-[200px]"
+      />
+      <!-- Flexbox-Container für die Icons oben rechts -->
+      <div class="flex justify-end">
+        <!-- MDI-Icon für den Account -->
+        <UIcon
+          name="i-heroicons-user-solid"
+          class="mr-2 cursor-pointer text-xl"
+          @click="handleAccountClick"
+        >
+        </UIcon>
+        <!-- MDI-Icon für den Logout -->
+        <UIcon
+          name="i-heroicons-arrow-right-start-on-rectangle-20-solid"
+          class="mr-2 cursor-pointer text-xl"
+          @click="handleLogoutClick"
+        >
+        </UIcon>
+      </div>
     </div>
     <!-- Flexbox-Container für die Anordnung -->
     <div class="flex h-full">
-    <!-- Seitenleiste -->
-    <div
-    class="w-[200px] bg-blue-900 p-5 flex flex-col h-full">
-    <!-- Inhalte der Seitenleiste -->
-    <UIcon name="i-heroicons-list-bullet-20-solid"
-    class="text-2xl mb-2 text-white cursor-pointer;" @click="handleListClick"></UIcon>
-    <!--Icons -->
-    <dic class="flex items-center mb-10 text-white cursor-pointer text-xl" @click="handelEditClick">
-    <UIcon name="i-heroicons-table-cells-solid" class="text-2xl mr-4 text-white" ></UIcon>
-    <span>Board</span>
-    </dic>
-    <div class="flex items-center mb-10 text-white cursor-pointer text-xl" @click="handelEditClick">
-    <UIcon name="i-heroicons-plus-circle-20-solid" class="text-2xl mr-4 text-white" ></UIcon>
-    <span>Add Ticket</span>
+      <!-- Seitenleiste -->
+      <div class="flex h-full w-[200px] flex-col bg-blue-900 p-5">
+        <!-- Inhalte der Seitenleiste -->
+        <UIcon
+          name="i-heroicons-list-bullet-20-solid"
+          class="cursor-pointer; mb-2 text-2xl text-white"
+          @click="handleListClick"
+        ></UIcon>
+        <!--Icons -->
+        <dic
+          class="mb-10 flex cursor-pointer items-center text-xl text-white"
+          @click="handelEditClick"
+        >
+          <UIcon
+            name="i-heroicons-table-cells-solid"
+            class="mr-4 text-2xl text-white"
+          ></UIcon>
+          <span>Board</span>
+        </dic>
+        <div
+          class="mb-10 flex cursor-pointer items-center text-xl text-white"
+          @click="handelEditClick"
+        >
+          <UIcon
+            name="i-heroicons-plus-circle-20-solid"
+            class="mr-4 text-2xl text-white"
+          ></UIcon>
+          <span>Add Ticket</span>
+        </div>
+        <div
+          class="mb-10 flex cursor-pointer items-center text-xl text-white"
+          @click="handelEditClick"
+        >
+          <UIcon
+            name="i-heroicons-chat-bubble-left-right-solid"
+            class="text 3xl mr-4 text-white"
+          ></UIcon>
+          <span>Discussion</span>
+        </div>
+        <div
+          class="mb-10 flex cursor-pointer items-center text-xl text-white"
+          @click="handelEditClick"
+        >
+          <UIcon
+            name="i-heroicons-clock-solid"
+            class="mr-4 text-3xl text-white"
+          ></UIcon>
+          <span>Pomodoro Timer</span>
+        </div>
+        <div
+          class="mb-10 flex cursor-pointer items-center text-xl text-white"
+          @click="handleEditClick"
+        >
+          <UIcon
+            name="i-heroicons-adjustments-horizontal"
+            class="mr-4 text-3xl text-white"
+          ></UIcon>
+          <span>Filter</span>
+        </div>
+      </div>
+      <table class="w-full flex-1">
+        <!-- Überschrift -->
+        <thead>
+          <tr class="h-10">
+            <th colspan="4" class="bg-blue-800 p-5 text-center text-white">
+              Kanban Board
+            </th>
+          </tr>
+          <!-- Spaltenüberschriften -->
+          <tr class="h-10 bg-blue-300">
+            <th class="flex-1 rounded p-5 text-white">Backlog</th>
+            <th class="flex-1 rounded p-5 text-white">To Do</th>
+            <th class="flex-1 rounded p-5 text-white">In Progress</th>
+            <th class="flex-1 rounded p-5 text-white">Done</th>
+          </tr>
+          <!-- Leere Spalten -->
+          <tr>
+            <td class="p-5"></td>
+            <td class="p-5"></td>
+            <td class="p-5"></td>
+            <td class="p-5"></td>
+          </tr>
+        </thead>
+      </table>
     </div>
-    <div class="flex items-center mb-10 text-white cursor-pointer text-xl" @click="handelEditClick">
-    <UIcon name="i-heroicons-chat-bubble-left-right-solid" class="text 3xl text-white mr-4"></UIcon>
-    <span>Discussion</span>
-    </div>
-    <div class="flex items-center mb-10 text-white cursor-pointer text-xl" @click="handelEditClick">
-    <UIcon name="i-heroicons-clock-solid" class="text-3xl text-white mr-4"></UIcon>
-    <span>Pomodoro Timer</span>
-    </div>
-    <div class="flex items-center mb-10 text-white cursor-pointer text-xl" @click="handleEditClick">
-    <UIcon name="i-heroicons-adjustments-horizontal" class=" text-3xl text-white  mr-4"></UIcon>
-    <span>Filter</span>
-    </div>
-    </div>
-    <table class="flex-1 w-full">
-    <!-- Überschrift -->
-    <thead>
-    <tr class="h-10">
-    <th colspan="4" class="p-5 bg-blue-800 text-white text-center">
-    Kanban Board
-    </th>
-    </tr>
-    <!-- Spaltenüberschriften -->
-    <tr class="bg-blue-300 h-10">
-    <th class="flex-1 text-white p-5 rounded">Backlog</th>
-    <th class="flex-1 text-white p-5 rounded">To Do</th>
-    <th class="flex-1 text-white p-5 rounded">In Progress</th>
-    <th class="flex-1 text-white p-5 rounded">Done</th>
-    </tr>
-    <!-- Leere Spalten -->
-    <tr>
-    <td class="p-5"></td>
-    <td class="p-5"></td>
-    <td class="p-5"></td>
-    <td class="p-5"></td>
-    </tr>
-    </thead>
-    </table>
-    </div>
-    </div>
-    </template>
+  </div>
+</template>
--- pages/loginsude.vue
+++ pages/loginsude.vue
@@ -1,35 +1,72 @@
 <template>
-    <div class="flex">
-      <div class="flex-1">
-        <img src="https://raw.githubusercontent.com/wdm373/MyKanbanBoard-Logo/6ac26beea3482351aa356ea9bf0b216a3a694f3b/Login-Seite.png" alt="Login Image">
-      </div>
-      <div class="flex-1 flex flex-col items-start p-8">
-        <h1 class="text-8xl font-bold mb-4 ml-24 mt-8"><span class="font-sans text-4xl">login</span></h1>
-        <h2 class="text-2xl font-bold mb-4 ml-40 mt-[-35px]"><span class="font-comic text-xl">MY KANBAN BOARD</span></h2>
-        <h3 class="text-2xl font-normal mb-12 ml-24 mt-[-4px] text-gray-600"><span class="font-sans text-lg">Welcome back! Please login to your account.</span></h3>
-        <form @submit.prevent="login" class="ml-24">
-  
-          <div class="flex flex-col mb-6">
-            <label for="username" class="text-xl font-semibold"><span class="font-sans text-sm text-gray-600"></span>Username</label>
-            <input type="text" id="username" v-model="username" placeholder="Enter your username" class="text-lg mb-12 border-2 border-gray-300 p-2 rounded-full w-80">
-          </div>
-          <div class="flex flex-col mb-2">
-            <label for="password" class="text-xl font-semibold"><span class="font-sans text-sm text-gray-600"></span>Password</label>
-            <input type="password" id="password" v-model="password" placeholder="Enter your password" class="text-lg mb-4 border-2 border-gray-300 p-2 rounded-full w-80">
-          </div>
-          <div class="flex items-center mb-4">
-            <input type="checkbox" id="rememberMe" v-model="rememberMe" class="mr-2">
-            <label for="rememberMe" class="text-xs text-gray-600">Remember Me</label>
-            <a href="#" class="text-xs text-gray-600 ml-32">Forgot Password?</a>
-          </div>
-          <button type="submit" class="text-lg mt-8 mb-4 ml-24 text-gray-600">New User? Sign Up</button>
-        </form>
+  <div class="flex">
+    <div class="flex-1">
+      <img src="../public/images/loginphoto.svg" alt="Login Image" />
+    </div>
+    <div class="flex flex-1 flex-col items-start p-8">
+      <h1 class="mb-4 ml-24 mt-8 text-8xl font-bold">
+        <span class="font-sans text-4xl">login</span>
+      </h1>
+      <h2 class="mb-4 ml-40 mt-[-35px] text-2xl font-bold">
+        <span class="font-comic text-xl">MY KANBAN BOARD</span>
+      </h2>
+      <h3 class="mb-12 ml-24 mt-[-4px] text-2xl font-normal text-gray-600">
+        <span class="font-sans text-lg"
+          >Welcome back! Please login to your account.</span
+        >
+      </h3>
+      <!--<form @submit.prevent="login" class="ml-24">
+        <div class="mb-6 flex flex-col">
+          <label for="username" class="text-xl font-semibold"
+            ><span class="font-sans text-sm text-gray-600"></span
+            >Username</label
+          >
+          <input
+            type="text"
+            id="username"
+            v-model="username"
+            placeholder="Enter your username"
+            class="mb-12 w-80 rounded-full border-2 border-gray-300 p-2 text-lg"
+          />
+        </div>
+        <div class="mb-2 flex flex-col">
+          <label for="password" class="text-xl font-semibold"
+            ><span class="font-sans text-sm text-gray-600"></span
+            >Password</label
+          >
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            placeholder="Enter your password"
+            class="mb-4 w-80 rounded-full border-2 border-gray-300 p-2 text-lg"
+          />
+        </div>
+        <div class="mb-4 flex items-center">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            v-model="rememberMe"
+            class="mr-2"
+          />
+          <label for="rememberMe" class="text-xs text-gray-600"
+            >Remember Me</label
+          >
+          <a href="#" class="ml-32 text-xs text-gray-600">Forgot Password?</a>
+        </div>
+        <button type="submit" class="mb-4 ml-24 mt-8 text-lg text-gray-600">
+          New User? Sign Up
+        </button>
+      </form>
+      -->
+      <div class="flex w-full justify-center">
+        <UButton size="xl" icon="i-mdi-github" @click="signIn()">Login</UButton>
       </div>
     </div>
-  </template>
-  
-  <script
-  lang="ts" setup>
+  </div>
+</template>
+
+<script lang="ts" setup>
 definePageMeta({
   layout: false,
 });
@@ -44,21 +81,4 @@ async function signIn() {
     },
   });
 }
-/*
-  export default {
-    data() {
-      return {
-        username: '',
-        password: '',
-        rememberMe: false
-      };
-    },
-    methods: {
-      login() {
-        
-      }
-    }
-  } */
-  </script>
-  
-  
+</script>
