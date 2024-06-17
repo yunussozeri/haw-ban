522518f95dfcd119a35a1a2d0c8afb8cdb3c3a2a
yunussozeri <yunus.sozeri.254@gmail.com>
2024-06-17 16:06:47 +0200

    fix middleware routing

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -8,7 +8,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
   //if user is not authenticated, send to login for auth
   if (!user.value) {
-    return navigateTo("/login");
+    return navigateTo("/");
   }
   // fetch when calling backend, from frontend use useFetch() from nuxt to get data from backend
   const dbUser = await $fetch("/api/user/", {
