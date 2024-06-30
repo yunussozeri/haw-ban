82bd49b704a53817f412277d0653985a17a47db6
Nicolas Wenzel <nicolas.l.wenzel@outlook.de>
2024-06-30 16:11:55 +0200

    redirect works again

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="hero min-h-screen" data-theme="winter">
     <div class="hero-content flex-col lg:flex-row-reverse">
-      <img src="../public/images/loginphoto.svg" class="max-w-sm rounded-lg shadow-2xl" />
+      <img
+        src="../public/images/loginphoto.svg"
+        class="max-w-sm rounded-lg shadow-2xl"
+      />
       <div>
         <h1 class="text-5xl font-bold">HAW-Ban</h1>
-        <p class="py-6">
-          Welcome to HAW-Ban! Please login to your account.
-        </p>
-        <UButton class="btn btn-primary" size="xl" icon="i-mdi-github" @click="signIn()">Login</UButton>
+        <p class="py-6">Welcome to HAW-Ban! Please login to your account.</p>
+        <UButton
+          class="btn btn-primary"
+          size="xl"
+          icon="i-mdi-github"
+          @click="signIn()"
+          >Login</UButton
+        >
       </div>
     </div>
   </div>
@@ -24,7 +31,7 @@ async function signIn() {
   client.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: "http://localhost:3000/",
+      redirectTo: "http://localhost:3000/board",
     },
   });
 }
